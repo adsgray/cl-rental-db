eec0f690d2e2592993dd98c8edbb2a85add7c642
Andrew Gray <adsgray@gmail.com>
2016-06-11 18:24:58 -0700

    S3 uploaddb.sh

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Fetch CL listings with bash and process them with python
 
-## To install BeautifilSoup
+## To install
 * sudo pip install beautifulsoup4
+
+## The sqlite3 file
+* updated daily and uploaded to S3
+* https://s3-us-west-1.amazonaws.com/adsgb02usw/cl-rental-db/cl.db
---
+++ src/uploaddb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# config.sh must:
+# export AWS_ACCESS_KEY_ID="KEY_ID"
+# export AWS_SECRET_ACCESS_KEY="ACCESS_KEY"
+. config.sh
+
+export S3_BUCKET="adsgb02usw"
+PREFIX="/home/ec2-user/code/craigslist-search/db"
+FOLDER="cl-rental-db"
+DBFILE="/home/ec2-user/code/craigslist-search/db/cl.db"
+
+s3put --prefix $PREFIX --key_prefix $FOLDER --bucket $S3_BUCKET $DBFILE
