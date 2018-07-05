3641b0cdfc905a9842f2e5764f5f116843834206
Andrew Gray <andrew.gray@hootsuite.com>
2018-07-05 14:02:13 -0700

    uploaddb: use aws cli and rely on instance creds

--- src/uploaddb.sh
+++ src/uploaddb.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-# config.sh must:
-# export AWS_ACCESS_KEY_ID="KEY_ID"
-# export AWS_SECRET_ACCESS_KEY="ACCESS_KEY"
-. config.sh
+# AWS creds (via key/secret or instance IAM role must allow:
+# # "s3:PutObject", "s3:PutObjectAcl" on the destination bucket/folder
 
-export S3_BUCKET="adsgb02usw"
-PREFIX="/home/ec2-user/code/craigslist-search/db"
+S3_BUCKET="adsgb02usw"
 FOLDER="cl-rental-db"
 DBFILE="/home/ec2-user/code/craigslist-search/db/cl.db"
 
-s3put --prefix $PREFIX --key_prefix $FOLDER --bucket $S3_BUCKET --grant public-read $DBFILE
+#s3put --prefix $PREFIX --key_prefix $FOLDER --bucket $S3_BUCKET --grant public-read $DBFILE
+aws s3 cp $DBFILE s3://$S3_BUCKET/$FOLDER/ --acl public-read"
