af6c9471536f68c0d1e0c83d61c8cd628aa697ef
Andrew Gray <andrew.gray@hootsuite.com>
2018-07-05 14:40:47 -0700

    fix paste typos

--- src/uploaddb.sh
+++ src/uploaddb.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # AWS creds (via key/secret or instance IAM role must allow:
-# # "s3:PutObject", "s3:PutObjectAcl" on the destination bucket/folder
+# "s3:PutObject", "s3:PutObjectAcl" on the destination bucket/folder
 
 S3_BUCKET="adsgb02usw"
 FOLDER="cl-rental-db"
 DBFILE="/home/ec2-user/code/craigslist-search/db/cl.db"
 
 #s3put --prefix $PREFIX --key_prefix $FOLDER --bucket $S3_BUCKET --grant public-read $DBFILE
-aws s3 cp $DBFILE s3://$S3_BUCKET/$FOLDER/ --acl public-read"
+aws s3 cp $DBFILE s3://$S3_BUCKET/$FOLDER/ --acl public-read
