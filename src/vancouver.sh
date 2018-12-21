5da708ab5dd63db79ca2b2f0578931c7e6831b30
Andrew Gray <andrew.gray@hootsuite.com>
2018-12-20 16:20:04 -0800

    use time instead of date for query

--- src/vancouver.sh
+++ src/vancouver.sh
@@ -1,2 +1,2 @@
 
-sqlite3 -separator $'\t' ~/Downloads/cl.db "select * from ad where loc1 in ( 'Vancouver','Coal Harbour','West End','East Vancouver','Yaletown','Kitsilano','Vancouver West Side', 'Cambie','South Vancouver','Downtown','Gastown','False Creek','Point Grey','Shaughnessy', 'Chinatown', 'UBC') and id > 153193" > vancouver.csv
+sqlite3 -separator $'\t' ~/Downloads/cl.db "select * from ad where loc1 in ( 'Vancouver','Coal Harbour','West End','East Vancouver','Yaletown','Kitsilano','Vancouver West Side', 'Cambie','South Vancouver','Downtown','Gastown','False Creek','Point Grey','Shaughnessy', 'Chinatown', 'UBC') and time > '2017-12-31'" > vancouver.csv
