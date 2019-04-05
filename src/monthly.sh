8f721c4f2403ab71b2628d18de1a2eff5f2ba23d
Andrew Gray <andrew.gray@hootsuite.com>
2019-04-05 11:27:39 -0700

    script clean up

--- src/monthly.sh
+++ src/monthly.sh
@@ -2,18 +2,6 @@
 
 vancouverloc="( 'Vancouver','Coal Harbour','West End','East Vancouver','Yaletown','Kitsilano','Vancouver West Side', 'Cambie','South Vancouver','Downtown','Gastown','False Creek','Point Grey','Shaughnessy', 'Chinatown', 'UBC')"
 furnished=0
-bedrooms="(0)"
-#from=2017-11-01
-#to=2017-12-01
-from=2018-12-01
-to=2019-01-01
-#from=2019-01-01
-#to=2019-02-01
-#from=2019-02-01
-#to=2019-03-01
-#from=2019-03-01
-#to=2019-04-01
-date="time >= '$from' and time <= '$to'"
 guardmin=100
 guardmax=10000
 file=/tmp/clrent.tsv
