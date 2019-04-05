b440e327c107abbc377f28fc74d448827bafb19d
Andrew Gray <andrew.gray@hootsuite.com>
2019-04-05 11:25:58 -0700

    update ugly median rent shell script

--- src/monthly.sh
+++ src/monthly.sh
@@ -24,10 +24,46 @@ do_median() {
 
 sqlite3 --separator $'\t' ~/Downloads/cl.db "select bedrooms, price, loc1, time from ad where bedrooms in $bedrooms and price > $guardmin and price < $guardmax and loc1 in $vancouverloc and furnished=$furnished and $date order by price;" > $file
 
-tail -n +$(((`cat $file | wc -l` / 2) + 1)) $file | head -n 1
+tail -n +$(((`cat $file | wc -l` / 2) + 1)) $file | head -n 1|awk '{print $2}'
 #wc -l $file
 }
 
+
+get_query_month() {
+    m=$1
+    if [ "$m" -lt "10" ]; then
+	echo "0$m"
+    else
+	echo "$m"
+    fi
+
+}
+
+# really ugly script to create a CSV of median rents
 for b in 0 1 2 3; do
-    do_median $b
+for year in 2016 2017 2018 2019; do
+	    for month in `seq 1 12`; do #1 2 3 4 5 6 7 8 9 10 11 12; do
+		y=$year
+		nextmonth=$(( $month + 1 ))
+		if [ "$nextmonth" -eq "13" ]; then
+		    nextmonth=1
+		    y=$(( $year + 1 ))
+		fi
+
+		qm=`get_query_month $month`
+		nqm=`get_query_month $nextmonth`
+		date="time >= '$year-$qm-01' and time < '$y-$nqm-01'"
+
+		echo -n "$year,$month,$b,"
+		do_median $b
+		echo ""
+	    done
 done
+done
+
+
+
+
+#for b in 0 1 2 3; do
+    #do_median $b
+#done
