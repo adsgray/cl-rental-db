c78e06cbe8e879269124cf3f72d5d89e28a0105d
Andrew Gray <adsgray@gmail.com>
2020-06-03 13:11:11 -0700

    separate furnished and unfurnished

--- src/monthly.sh
+++ src/monthly.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 vancouverloc="( 'Vancouver','Coal Harbour','West End','East Vancouver','Yaletown','Kitsilano','Vancouver West Side', 'Cambie','South Vancouver','Downtown','Gastown','False Creek','Point Grey','Shaughnessy', 'Chinatown', 'UBC')"
-furnished=0
 guardmin=100
 guardmax=10000
 file=/tmp/clrent.tsv
@@ -9,8 +8,11 @@ file=/tmp/clrent.tsv
 do_median() {
     bedrooms=$1
     bedrooms="($bedrooms)"
+    furnished=$2
 
 sqlite3 --separator $'\t' ~/Downloads/cl.db "select bedrooms, price, loc1, time from ad where bedrooms in $bedrooms and price > $guardmin and price < $guardmax and loc1 in $vancouverloc and furnished=$furnished and $date order by price;" > $file
+
+#sqlite3 --separator $'\t' ~/Downloads/cl.db "select bedrooms, price, loc1, time from ad where bedrooms in $bedrooms and price > $guardmin and price < $guardmax and loc1 in $vancouverloc and $date order by price;" > $file
 
 count=`wc -l $file|awk '{print $1}'`
 echo -n "$count,"
@@ -31,7 +33,7 @@ get_query_month() {
 
 # really ugly script to create a CSV of median rents
 for b in 0 1 2 3; do
-for year in 2016 2017 2018 2019; do
+for year in 2016 2017 2018 2019 2020; do
 	    for month in `seq 1 12`; do #1 2 3 4 5 6 7 8 9 10 11 12; do
 		y=$year
 		nextmonth=$(( $month + 1 ))
@@ -44,9 +46,11 @@ for year in 2016 2017 2018 2019; do
 		nqm=`get_query_month $nextmonth`
 		date="time >= '$year-$qm-01' and time < '$y-$nqm-01'"
 
-		echo -n "$year,$month,$b,"
-		do_median $b
-		echo ""
+		for furn in 0 1; do
+		    echo -n "$year,$month,$b,$furn,"
+		    do_median $b $furn
+		    echo ""
+		done
 	    done
 done
 done
